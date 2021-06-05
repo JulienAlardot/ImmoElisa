dc23cfb99d250576c44bbbe3a91cd41dc6b194be
Julien Alardot <alardotj.pro@gmail.com>
2021-06-05 02:46:53 +0200

    Editing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 mkdir -p ~/.streamlit/echo "\
+[global]\n\
+showWarningOnDirectExecution = false\n\
+\n\
+[theme]\n\
+base=\"dark\"\n\
+\n\
+[logger]\n\
+level=\"info\"\n\
+\n\
+[client]\n\
+caching = true\n\
+\n\
 [server]\n\
 headless = true\n\
+runOnSave = true\n\
 port = $PORT\n\
 enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
