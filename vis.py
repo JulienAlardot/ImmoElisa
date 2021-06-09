f6c41a2c4f033840fde5763022c58eee5494bf50
Julien Alardot <alardotj.pro@gmail.com>
2021-06-09 13:35:29 +0200

    Updating layout

--- vis.py
+++ vis.py
@@ -175,7 +175,6 @@ async def main():
     st.header("About the project")
     st.markdown(
         '''
-        ---
             
         <div align='justify'>
             
@@ -183,7 +182,8 @@ async def main():
         
         <div align='justify'>
             
-        This project was made during an AI formation at [Becode](https://becode.org/). The main goals of the project were :
+        This project was made during an AI formation at [Becode](https://becode.org/). The main goals of the project 
+        were :
         
         1. Create a new database by scrapping real-estate websites
         2. Clean and analyse the database
@@ -191,20 +191,22 @@ async def main():
         4. Find the best algorithm and search for the optimum parameters 
             
         The different repositories for each step of the projects are:
+        
         1. [Database Creation](https://github.com/JulienAlardot/challenge-collecting-data)
         2. [Data Analysis](https://github.com/JulienAlardot/ImmoElizaVisu)
         3. [Machine learning preprocessing and Deployment (this step)](https://github.com/JulienAlardot/ImmoElisa)
         
-        The challenges for this project are extracting the necessary features in a way that would be useful for the algorithms.
-        To fill  the 
-        gaps in the data without negatively impacting the algorithm ability to predict from new data. And to deploy the 
-        algorithm and automatise the whole process to continuously update the database and increase the algorithm ability to 
-        predict .
+        The challenges for this project are extracting the necessary features in a way that would be useful for the 
+        algorithms. To fill the gaps in the data without negatively impacting the algorithm ability to predict from new 
+        data. And to deploy the algorithm and automatise the whole process to continuously update the database and 
+        increase the algorithm ability to predict .
         
-        The previous steps in this project were made with different teams. See individual repositories for more informations 
-        about the teams.
+        The previous steps in this project were made with different teams. See individual repositories for more 
+        informations about the teams.
             
         </div>
+        
+        ---
             
         ''', unsafe_allow_html=True
     )
@@ -212,7 +214,6 @@ async def main():
     st.header("About the author")
     st.markdown(
         '''
-        ---
 
         <div align='justify'>
 
@@ -233,6 +234,7 @@ async def main():
         ## Links
         
         [Github](https://github.com/JulienAlardot)
+        
         [LinkedIn](https://www.linkedin.com/in/julien-alardot-84151a150/)
         
         </div>
